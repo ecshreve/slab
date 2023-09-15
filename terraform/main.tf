b086be61e28505433843b4942d1a61a66534efb9
shreve <eric@shreve.dev>
2023-09-15 12:29:58 -0700

    fix: fix module params

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,17 +14,12 @@ terraform {
   }
 }
 
-# variable "ssh_key" {
-#   type      = string
-#   sensitive = true
-# }
-
 provider "proxmox" {
   pm_tls_insecure     = true
 }
 
 module "devbox" {
-  count  = 1
+  count  = 0
   source = "app.terraform.io/slablan/ubuntu-vm/proxmox"
   version = "1.1.3"
 
@@ -36,46 +31,3 @@ module "devbox" {
   mem   = 8196
   disk  = "20G"
 }
-
-# resource "proxmox_vm_qemu" "dbox" {
-#   // Configure general VM parameters
-#   count       = 0
-#   target_node = "srv-1"
-#   name        = "devbox1"
-#   vmid        = 5100
-#   clone       = "ubuntu-jammy-server-cloudimg-tmpl"
-#   os_type     = "cloud-init"
-#   agent       = 1
-
-#   // Configure resources for the new VM
-#   cpu      = "host"
-#   sockets  = 1
-#   cores    = 2
-#   memory   = 4096
-#   scsihw   = "virtio-scsi-pci"
-#   bootdisk = "scsi0"
-
-#   // Configure disk resource
-#   disk {
-#     size    = "12G"
-#     type    = "scsi"
-#     storage = "local-lvm"
-#   }
-
-#   network {
-#     model  = "virtio"
-#     bridge = "vmbr0"
-#   }
-
-#   lifecycle {
-#     ignore_changes = [
-#       network,
-#     ]
-#   }
-
-#   // Configure cloud init params like IP address, DNS servers, user, etc
-#   ipconfig0    = "ip=10.40.10.100/24,gw=10.40.10.1"
-#   ciuser       = "root"
-#   cipassword   = "password"
-#   sshkeys      = var.ssh_key
-# }
