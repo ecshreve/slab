47691cfaea99b9e11206833fa5134f86e33a3804
shreve <eric@shreve.dev>
2023-09-15 12:07:52 -0700

    feat: tf via cli works

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,4 @@
 terraform {
-  cloud {
-    organization = "slablan"
-
-    workspaces {
-      name = "slab"
-    }
-  }
   required_providers {
     proxmox = {
       source  = "Telmate/proxmox"
@@ -14,19 +7,54 @@ terraform {
   }
 }
 
-provider "proxmox" {
-  pm_tls_insecure = true
+variable "ssh_key" {
+  type      = string
+  sensitive = true
 }
 
-module "ubuntu_vm" {
-  count  = 1
-  source = "app.terraform.io/slablan/ubuntu-vm/proxmox"
-  version = "1.1.3"
+provider "proxmox" {
+  pm_tls_insecure     = true
+}
 
-  name       = "devbox"
-  vmid       = 5000
-  cores      = 2
-  mem        = 4096
-  disk       = "24G"
-  ip_address = "10.40.40.200"
+resource "proxmox_vm_qemu" "dbox" {
+  // Configure general VM parameters
+  count       = 1
+  target_node = "srv-1"
+  name        = "devbox1"
+  vmid        = 5100
+  clone       = "ubuntu-jammy-server-cloudimg-tmpl"
+  os_type     = "cloud-init"
+  agent       = 1
+
+  // Configure resources for the new VM
+  cpu      = "host"
+  sockets  = 1
+  cores    = 2
+  memory   = 4096
+  scsihw   = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+
+  // Configure disk resource
+  disk {
+    size    = "12G"
+    type    = "scsi"
+    storage = "local-lvm"
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+
+  // Configure cloud init params like IP address, DNS servers, user, etc
+  ipconfig0    = "ip=10.40.10.100/24,gw=10.40.10.1"
+  ciuser       = "root"
+  cipassword   = "password"
+  sshkeys      = var.ssh_key
 }
