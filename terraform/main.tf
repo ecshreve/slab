986193bfc09b6c2164766d7e99d8bfe1a2dce357
shreve <eric@shreve.dev>
2023-09-15 12:36:11 -0700

    feat: create devbox

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,14 +19,13 @@ provider "proxmox" {
 }
 
 module "devbox" {
-  count  = 0
+  count  = 1
   source = "app.terraform.io/slablan/ubuntu-vm/proxmox"
   version = "1.1.4"
 
   name = "devbox"
-  vmid = 5101
+  vmid = 501
 
-  cloneimg = "ubuntu-jammy-server-cloudimg-tmpl"
   ip_address = "10.14.40.100"
   mem   = 8196
   disk  = "20G"
