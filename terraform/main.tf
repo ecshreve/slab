3dd937c9f889de14895f47228d6b7a8ef105ec8f
shreve <eric@shreve.dev>
2023-09-15 14:05:29 -0700

    fix: use local state

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,4 @@
 terraform {
-  cloud {
-    organization = "slablan"
-
-    workspaces {
-      name = "slab"
-    }
-  }
   required_providers {
     proxmox = {
       source  = "Telmate/proxmox"
@@ -19,24 +12,13 @@ provider "proxmox" {
 }
 
 module "devbox" {
-  count  = 0
-  source = "app.terraform.io/slablan/ubuntu-vm/proxmox"
-  version = "1.1.4"
-
-  name = "devbox"
-  vmid = 501
-
-  ip_address = "10.14.40.100"
-  mem   = 8196
-  disk  = "20G"
-}
-
-module "dbox" {
   count  = 1
   source = "app.terraform.io/slablan/ubuntu-vm/proxmox"
   version = "1.1.5"
 
-  name = "dbox"
-  vmid = 601
-  ip_address = "dhcp"
+  name = "devbox"
+  vmid = 800
+  ip_address = "10.14.40.80/24"
+  mem = 8192
+  disk = "24G"
 }
