d3401354513e49982798c7f01638de7f9c4b2a4f
eric <eric@shreve.dev>
2023-09-19 20:11:45 +0000

    tmpagain

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,3 +23,16 @@ module "cluster" {
   mem = 4096
   # pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMu95UhPrZHh/+0O0v6QCn46VodoT7RLZDRlGP6rTWW2"
 }
+
+module "cluster2" {
+  count  = 1
+  source = "app.terraform.io/slablan/ubuntu-vm/proxmox"
+  version = "1.1.5"
+
+  name = "node-1${count.index}"
+  vmid = 160 + count.index
+  ip_address = "10.14.40.16${count.index}/24"
+  cores = 2
+  mem = 4096
+  pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMu95UhPrZHh/+0O0v6QCn46VodoT7RLZDRlGP6rTWW2"
+}
