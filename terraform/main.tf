8aba8b0d46278b9ceb069f792ca23e301f8fcb13
eric <eric@shreve.dev>
2023-09-20 04:52:15 +0000

    tmp

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,23 @@ module "cluster" {
 
   name = "node-${count.index}"
   vmid = 140 + count.index
+
   ip_address = "10.14.40.14${count.index}/24"
   cores = 2
   mem = 8192
+}
+
+module "registry" {
+  count  = 1
+  source = "app.terraform.io/slablan/ubuntu-vm/proxmox"
+  version = "1.1.5"
+
+  name = "docker-registry"
+  vmid = 5000
+
+  ip_address      = "10.14.40.185/24"
+
+  cores = 2
+  mem   = 2048
+  disk  = "50G"
 }
